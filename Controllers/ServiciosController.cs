d53f0665a18ddee90ea191d43c76815a2c71585d
Ignacio <ignacioprieto6@gmail.com>
2024-09-15 15:50:46 -0300

    agregado historial de comentarios

--- Controllers/ServiciosController.cs
+++ Controllers/ServiciosController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SpaWebApp.Models;
+using SpaWebApp.Data;
+using System.Linq;
 
 namespace SpaWebApp.Controllers
 {
     public class ServiciosController : Controller
     {
+        private readonly SpaContext _context;
+
+        public ServiciosController(SpaContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var comentarios = _context.Comentarios.ToList();
+            return View(comentarios);
         }
     }
 }
